815184dcc1ad2fc2dcd2b0616730094778c4dcac
zeyu8023 <166600771+zeyu8023@users.noreply.github.com>
2024-09-11 11:19:29 +0800

    Update frps_linux_install.sh

--- frps_linux_install.sh
+++ frps_linux_install.sh
@@ -12,8 +12,8 @@ Font="\033[0m"
 # fonts color
 
 # variable
-FRP_VERSION=0.60.0
-REPO=stilleshan/frps
+FRP_VERSION="0.28.2"
+REPO=zeyu8023/frps
 WORK_PATH=$(dirname $(readlink -f $0))
 FRP_NAME=frps
 FRP_PATH=/usr/local/frp
